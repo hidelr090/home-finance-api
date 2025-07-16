316c7760a58f42cdabff1260a118ac0b05c63b5f
hidelr090 <hidelgardo503@gmail.com>
2025-07-16 18:25:22 -0300

    feat: auth middlweare working

--- app/shared/middlewares/auth.py
+++ app/shared/middlewares/auth.py
@@ -3,24 +3,25 @@ from fastapi import Request, HTTPException, status, Depends
 from datetime import datetime, timezone
 from app.repositories.user_repository import UserRepository
 from app.factories.repositories.user_repository_factory import user_repository_factory
-from app.shared.utils.cryptography import decode
+from app.shared.utils.cryptography import decode as decoder
 
 async def auth_middleware(
     request: Request,
     user_repository: UserRepository = Depends(user_repository_factory)
 ):
-    auth_header = request.headers.get("Authorization")
+    auth_header = request.headers.get("authorization")
     if not auth_header or not auth_header.startswith("Bearer "):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Missing or invalid token")
 
-    token = auth_header.removeprefix("Bearer ").strip()
+    token = auth_header.split(" ")[1].strip()
     
-    user_id = decode(token).id
-    user = await user_repository.get_by_id(user_id)
+    decoded_token = decoder(token)
+    user_id = decoded_token.get("id") or decoded_token.get("id_1", {}).get("id")
+    user = user_repository.get_by_id(user_id)
     if not user:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
-    if not user.token_expiration or user.token_expiration < datetime.now(timezone.utc):
+    if not user.token_expiration or user.token_expiration.replace(tzinfo=timezone.utc) < datetime.now(timezone.utc):
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Token expired")
 
     return user
